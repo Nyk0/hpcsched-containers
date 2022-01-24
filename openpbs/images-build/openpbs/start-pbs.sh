0e9b10495de394c60124a99539ba3186437324a0
root <root@admin.lab.local>
2022-01-24 23:57:42 +0100

    fix

--- openpbs/images-build/openpbs/start-pbs.sh
+++ openpbs/images-build/openpbs/start-pbs.sh
@@ -2,28 +2,17 @@
 
 declare -a progs=("pbs_comm" "pbs_sched" "pbs_ds_monitor" "pbs_server.bin")
 
-PBS_SERVER=`grep PBS_SERVER /etc/pbs.conf | cut -f 2 -d '='`
-
-while true
-do
-	pg_isready -h $PBS_SERVER -p 15007
-	if [ $? == 0 ]
-	then
-		break
-	fi
-	sleep 2
-done
+/opt/pbs/libexec/pbs_habitat
 
 /opt/pbs/sbin/pbs_comm
 /opt/pbs/sbin/pbs_sched
-/opt/pbs/sbin/pbs_ds_monitor monitor
 /opt/pbs/sbin/pbs_server
 
 while true
 do
 	for prog in "${progs[@]}"
 	do
-		pidof $prog
+		pidof $prog > /dev/null 2>&1
 		if [ $? -ne 0 ]
 		then
 			exit 1
--- openpbs/images-build/openpbs/start-postgres.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/opt/pbs/libexec/pbs_habitat
-su - postgres -c '/usr/lib/postgresql/13/bin/postgres -D /var/spool/pbs/datastore -p 15007'
