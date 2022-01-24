3e0afca38064f4a1da8fb40280e81f4291bf04a4
root <root@debian.test.local>
2022-01-24 18:10:59 +0100

    fix

---
+++ openpbs/images-build/openpbsinit/generate_pbs_conf.py
@@ -0,0 +1,41 @@
+from kubernetes import client, config
+import socket
+import time
+import subprocess
+
+def locate_db():
+	db_server = False
+	config.load_incluster_config()
+	v1 = client.CoreV1Api()
+	ret = v1.list_namespaced_pod("hpc-nico", label_selector="role=control-node")
+
+	for i in ret.items:
+		db_server = i.metadata.name
+	return db_server
+
+def generate(db_server):
+	oar_conf_file = (f'PBS_SERVER=' + db_server + '\n'
+	f'PBS_START_SERVER=1\n'
+	f'PBS_START_SCHED=1\n'
+	f'PBS_START_COMM=1\n'
+	f'PBS_START_MOM=0\n'
+	f'PBS_EXEC=/opt/pbs\n'
+	f'PBS_HOME=/var/spool/pbs\n'
+	f'PBS_CORE_LIMIT=unlimited\n'
+	f'PBS_SCP=/usr/bin/scp')
+
+	return oar_conf_file
+
+def write_conf(var,path):
+	f = open(path,'w')
+	for line in var.splitlines():
+		f.write(line + "\n")
+	f.close
+
+if __name__ == '__main__':
+	db_server = False
+
+	while db_server == False:
+		db_server = locate_db()
+
+	write_conf(generate(db_server),"/pbsconf/pbs.conf")
