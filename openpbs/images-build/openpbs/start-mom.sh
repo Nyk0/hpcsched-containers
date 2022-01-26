d1f00107fdf5611d6c49b3361b941d8ce7362e2c
root <root@admin.lab.local>
2022-01-26 13:18:42 +0100

    fix

--- openpbs/images-build/openpbs/start-mom.sh
+++ openpbs/images-build/openpbs/start-mom.sh
@@ -2,4 +2,13 @@
 
 source /etc/profile.d/pbs.sh
 
+PBS_CONTROLLER=`grep PBS_SERVER /etc/pbs.conf | cut -d "=" -f 2`
+
+reach_slurmctld=1
+while [ $reach_slurmctld != 0 ]
+do
+	nc -z $PBS_CONTROLLER 15001 > /dev/null 2>&1
+	reach_slurmctld=$?
+done
+
 /opt/pbs/sbin/pbs_mom -N
